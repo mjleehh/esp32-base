84b740ec7260afa8c53467db10aa1bba20f42e24
Michael Jonathan Lee <code.mjlee@gmail.com>
2019-06-12 12:09:09 +0200

    One handler to rule them all.

--- mfl_http/include/mfl/httpd/Router.hpp
+++ mfl_http/include/mfl/httpd/Router.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <mfl/httpd/HttpLayer.hpp>
-#include <mfl/httpd/Context.hpp>
+#include <mfl/httpd/getHandlerArgTypes.hpp>
 #include <mfl/httpd/PathNode.hpp>
 #include <mfl/http/Method.hpp>
 #include <mfl/http/response_types/response-types.hpp>
@@ -28,8 +28,8 @@ struct Router {
         addHandler(http::Method::get, uriTemplate, handler);
     }
 
-    template<typename T>
-    void put(const std::string& uriTemplate, const Handler<T>& handler) {
+    template<typename F>
+    void put(const std::string& uriTemplate, F handler) {
         addHandler(http::Method::put, uriTemplate, handler);
     }
 
@@ -40,16 +40,18 @@ struct Router {
 
     void handle(Context<std::string>& context) const;
 private:
-    template<typename T>
-    void addHandler(http::Method method, const std::string& uriTemplate, const Handler<T>& handler) {
+    template<typename F>
+    void addHandler(http::Method method, const std::string& uriTemplate, F handler) {
         addPlainHandler(method, uriTemplate, [handler](Context<std::string>& c){
-            T body = http::response_types::deserialize<T>(c.body);
-            Context<T> newC;
+            using RequestBodyT = typename getHandlerArgTypes<F>::request_body_type;
+            using ResponseBodyT = typename getHandlerArgTypes<F>::response_body_type;
+            auto body = http::response_types::deserialize<RequestBodyT>(c.body);
+            Context<RequestBodyT, ResponseBodyT> newC;
             newC.method = c.method;
             newC.uri = c.uri;
             newC.body = body;
             handler(newC);
-            c.res.body = http::response_types::serialize<T>(newC.res.body);
+            c.res.body = http::response_types::serialize<ResponseBodyT>(newC.res.body);
         });
     }
 
---
+++ mfl_http/include/mfl/httpd/getHandlerArgTypes.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <mfl/httpd/Context.hpp>
+#include <type_traits>
+
+namespace mfl::httpd {
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+template<class A, class B>
+struct indirect_false {
+    static const bool value = false;
+};
+
+template<class A>
+struct invalid_handler_function {
+    using type = std::nullptr_t;
+    static_assert(indirect_false<A, A>::value, "passed an invalid handler function");
+};
+
+// =====================================================================================================================
+
+template <typename F>
+struct getFunctorTypeArgs : getFunctorTypeArgs<decltype(&F::operator())> {};
+
+template <typename FunctorT, typename RequestBodyT, typename ResponseBodyT>
+struct getFunctorTypeArgs<void(FunctorT::*)(Context<RequestBodyT, ResponseBodyT>&) const> {
+    using request_body_type  = RequestBodyT;
+    using response_body_type = ResponseBodyT;
+};
+
+// =====================================================================================================================
+
+template <typename FunctorT, typename RequestBodyT, typename ResponseBodyT>
+struct getFunctorTypeArgs<void(FunctorT::*)(Context<RequestBodyT, ResponseBodyT>&)> {
+    using request_body_type  = RequestBodyT;
+    using response_body_type = ResponseBodyT;
+};
+
+// =====================================================================================================================
+
+template<class F, bool isFunction>
+struct getHandlerFunctionTypeArgs {
+    using request_body_type = typename invalid_handler_function<F>::type;
+};
+
+template<class RequestBodyT, class ResponseBodyT>
+struct getHandlerFunctionTypeArgs<void(*)(Context<RequestBodyT,ResponseBodyT>&), true> {
+    using request_body_type  = RequestBodyT;
+    using response_body_type = ResponseBodyT;
+};
+
+template<class F>
+struct getHandlerFunctionTypeArgs<F, false> : getFunctorTypeArgs<F> {};
+
+// =====================================================================================================================
+
+template<class F>
+struct getHandlerArgTypes
+        : getHandlerFunctionTypeArgs<F, std::is_function<typename std::remove_pointer<F>::type>::value> {};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+}
