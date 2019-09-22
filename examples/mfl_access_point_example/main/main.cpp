965518f68c1b8d9599ff3f989daeb3ee50836c47
Michael Jonathan Lee <code.mjlee@gmail.com>
2019-09-22 16:39:06 +0200

    Use config values for access point example.

--- examples/mfl_access_point_example/main/main.cpp
+++ examples/mfl_access_point_example/main/main.cpp
@@ -1,12 +1,13 @@
-
 #include <freertos/portmacro.h>
 #include <mfl/AccessPoint.hpp>
 #include <esp_log.h>
 
+#include <sdkconfig.h>
+
 const char* tag = "main";
 
 extern "C" void app_main() {
-    mfl::AccessPoint accessPoint("myAP", "esp32testAP", "verySecret");
+    mfl::AccessPoint accessPoint(CONFIG_DEVICE_HOSTNAME, CONFIG_AP_NAME, CONFIG_AP_KEY);
     accessPoint.start([]{
         ESP_LOGI(tag, "acces point started");
     });
