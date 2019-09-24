bcc04b9778c90130e6720c78d9456e0b335ff734
Michael Jonathan Lee <code.mjlee@gmail.com>
2019-09-24 20:04:41 +0200

    Fixed missing nvs storage init for AP example.

--- examples/mfl_access_point_example/main/main.cpp
+++ examples/mfl_access_point_example/main/main.cpp
@@ -1,12 +1,16 @@
 #include <freertos/portmacro.h>
 #include <mfl/AccessPoint.hpp>
 #include <esp_log.h>
+#include <nvs_flash.h>
 
 #include <sdkconfig.h>
 
 const char* tag = "main";
 
 extern "C" void app_main() {
+    nvs_flash_init();
+    tcpip_adapter_init();
+
     mfl::AccessPoint accessPoint(CONFIG_DEVICE_HOSTNAME, CONFIG_AP_NAME, CONFIG_AP_KEY);
     accessPoint.start([]{
         ESP_LOGI(tag, "acces point started");
